21ed355b000705730b5017f77b80ff462203d5e6
Ben Balter <ben.balter@github.com>
2016-10-30 15:59:45 -0400

    fix last error

--- jekyll-export-cli.php
+++ jekyll-export-cli.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Run the exporter from the command line and spit the zipfile to STDOUT.
+ * Exports WordPress posts, pages, and options as YAML files parsable by Jekyll
  *
  * @package    JekyllExporter
  * @author     Ben Balter <ben.balter@github.com>
@@ -9,6 +9,13 @@
  * @link       https://github.com/benbalter/wordpress-to-jekyll-exporter/
  */
 
+/**
+ * Usage:
+ *
+ *     $ php jekyll-export-cli.php > my-jekyll-files.zip
+ *
+ * Must be run in the wordpress-to-jekyll-exporter/ directory.
+ */
 include '../../../wp-load.php';
 include '../../../wp-admin/includes/file.php';
 require_once 'jekyll-exporter.php'; // Ensure plugin is "activated".
@@ -17,12 +24,5 @@ if ( php_sapi_name() != 'cli' ) {
 	wp_die( 'Jekyll export must be run via the command line or administrative dashboard.' );
 }
 
-/**
-* Usage:
-*
-*     $ php jekyll-export-cli.php > my-jekyll-files.zip
-*
-* Must be run in the wordpress-to-jekyll-exporter/ directory.
-*/
 $jekyll_export = new Jekyll_Export();
 $jekyll_export->export();
