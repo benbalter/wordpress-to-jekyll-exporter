0bc1ddf8979a87809c100db14de0c59f09bb535e
Ben Balter <ben.balter@github.com>
2016-10-30 14:37:18 -0400

    update test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,25 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package wordpress-to-jekyll-exporter
+ */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../jekyll-exporter.php';
+	require dirname( dirname( __FILE__ ) ) . '/wordpress-to-jekyll-exporter.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
