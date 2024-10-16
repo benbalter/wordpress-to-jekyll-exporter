1f20928e240cb2617929647d7f41b8ae1ae85172
Ben Balter <ben@balter.com>
2024-10-16 15:31:22 +0000

    bump version

--- jekyll-exporter.php
+++ jekyll-exporter.php
@@ -12,9 +12,9 @@
  * Plugin Name: WordPress to Jekyll Exporter
  * Plugin URI:  https://github.com/benbalter/wordpress-to-jekyll-exporter/
  * Description: Exports WordPress posts, pages, and options as YAML files parsable by Jekyll
- * Version:     2.4.0
+ * Version:     2.4.1
  * Author:      Ben Balter
- * Author URI:  http://ben.balter.com
+ * Author URI:  https://ben.balter.com
  * Text Domain: jekyll-export
  * License:     GPL-3.0+
  * License URI: http://www.gnu.org/licenses/gpl-3.0.txt
